371c9e8db075062799aff6d07235747280c166d4
Ismael Neto <114830206+otaldoneto@users.noreply.github.com>
2026-09-25 11:17:04 -0300

    chore: add PostgreSQL and Redis with Docker Compose

--- src/test/java/com/urlshortener/TestcontainersConfiguration.java
+++ src/test/java/com/urlshortener/TestcontainersConfiguration.java
@@ -13,13 +13,13 @@ class TestcontainersConfiguration {
 	@Bean
 	@ServiceConnection
 	PostgreSQLContainer postgresContainer() {
-		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+		return new PostgreSQLContainer(DockerImageName.parse("postgres:18"));
 	}
 
 	@Bean
 	@ServiceConnection(name = "redis")
 	GenericContainer<?> redisContainer() {
-		return new GenericContainer<>(DockerImageName.parse("redis:latest")).withExposedPorts(6379);
+		return new GenericContainer<>(DockerImageName.parse("redis:8")).withExposedPorts(6379);
 	}
-
 }
+
